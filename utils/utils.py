20e51a8104fcc98c3ffc7e2d1e7bee5a6096a811
BrennenChadburn <brennen.chadburn@northwesternanalytics.com>
2021-11-11 10:51:11 -0700

    Print # of GPU/CPU test runs

--- utils/utils.py
+++ utils/utils.py
@@ -50,10 +50,10 @@ def time_it(func):
 
 
 def print_system_info():
-    def format_header(header, limit=79):
-        Left_margin = int(round((limit - len(header) - 2) / 2, 0))
-        Right_margin = int(limit - Left_margin - len(header) - 2)
-        output = "=" * (Left_margin) + " " + header + " " + "=" * (Right_margin)
+    def format_header(header, limit=80):
+        left_margin = int(round((limit - len(header) - 2) / 2, 0))
+        right_margin = int(limit - left_margin - len(header) - 2)
+        output = "=" * (left_margin) + " " + header + " " + "=" * (right_margin)
         print(output)
 
     format_header("Package Versions")
@@ -74,22 +74,26 @@ def limit_memory(enable=True):
     try:
         for device in gpu_devices:
             tf.config.experimental.set_memory_growth(device, enable=enable)
-    except (RuntimeError):
-        print("Device can only be configured prior to initialization. Please restart kernal.")
+    except RuntimeError:
+        print("Device can only be configured prior to initialization. Please restart kernel.")
+
+
+def create_conv_layer():
+    random_image_cpu = tf.random.normal((100, 100, 100, 3))
+    net_cpu = tf.keras.layers.Conv2D(32, 7)(random_image_cpu)
+    return net_cpu
 
 
 def cpu():
     with tf.device('/cpu:0'):
-        random_image_cpu = tf.random.normal((100, 100, 100, 3))
-        net_cpu = tf.keras.layers.Conv2D(32, 7)(random_image_cpu)
+        net_cpu = create_conv_layer()
         return tf.math.reduce_sum(net_cpu)
 
 
 def gpu():
     with tf.device('/device:GPU:0'):
-        random_image_gpu = tf.random.normal((100, 100, 100, 3))
-        net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
-        return tf.math.reduce_sum(net_gpu)
+        net_cpu = create_conv_layer()
+        return tf.math.reduce_sum(net_cpu)
 
 
 def warm_up(devices):
@@ -105,7 +109,7 @@ def warm_up(devices):
 def get_processor_time(devices=["GPU", "CPU"], runs=10):
     warm_up(devices)
     print(f'Time (s) to convolve 32x7x7x3 filter over random 100x100x100x3 images '
-          '(batch x height x width x channel). Sum of {runs} runs.')
+          f'(batch x height x width x channel). Total of {runs} runs.')
     cpu_exp_time = timeit.timeit('cpu()',
                                  number=runs,
                                  setup="from __main__ import cpu")
@@ -122,3 +126,4 @@ def check_cpu_gpu():
     print_system_info()
     limit_memory(enable=True)
     get_processor_time()
+
